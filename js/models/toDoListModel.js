e521faa255c6e511ea5793c0ed16b474b8381c70
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-03-22 13:23:57 -0600

    Edit Functionality

---
+++ js/models/toDoListModel.js
@@ -0,0 +1,43 @@
+import { ref, set, get, push, child, remove, update } from "firebase/database";
+import { db } from "../lib/firebase/config/firebaseInit";
+import { createStore, removeFromStore, updateStore } from "./store";
+
+let observers = []
+
+export function subscribe(fn) {
+    observers.push(fn)
+    console.log(observers)
+}
+
+export function notify(data) {
+    observers.forEach((observer) => observer(data))
+}
+
+export async function getToDoData() {
+    const dbRef = ref(db, 'todos')
+    const response = await get(dbRef)
+    let payload = await response.val()
+    payload = Object.entries(payload)
+    let toDoItems = payload.map((item) => {
+        return {...item[1], uid: item[0]}
+    })
+    if (await createStore(toDoItems)) {
+        notify(toDoItems)
+    }
+
+}
+
+export function deleteToDo(uid) {
+    const dbRef = ref(db, `todos/${uid}`)
+    remove(dbRef)
+    const store = removeFromStore(uid)
+    notify(store)
+}
+
+export function updateToDo(updatedToDo) {
+    let payload = updatedToDo
+    const dbRef = ref(db, `todos/${payload.uid}`)
+    update(dbRef, payload)
+    const store = updateStore(payload)
+    notify(store)
+}
---
+++ js/views/toDoListView.js
@@ -0,0 +1,42 @@
+import { toDoItemTemplate } from "../templates/toDoItemTemplate";
+import { subscribe } from "../models/toDoListModel";
+import { deleteToDoController } from "../controllers/deleteToDoController"
+import { updateToDoController } from "../controllers/updateToDoController";
+
+let view;
+
+
+export function toDoListView() {
+    view = document.querySelector('#to-do-list')
+    view.addEventListener('click', onHandleClick)
+    // render()
+}
+
+subscribe(render)
+
+function render(data) {
+    const div = document.createElement('div')
+    const toDoList = document.querySelector('#item-container')
+    toDoList.replaceChildren()
+    // const testToDo = div.prepend(toDoItemTemplate(testObject))
+
+    data.forEach((item) => {
+        div.prepend(toDoItemTemplate(item))
+    })
+
+    toDoList.append(div)
+}
+
+function onHandleClick(e) {
+    switch (e.target.id) {
+        case 'delete':
+            deleteToDoController(e.target.dataset.uid)
+            break
+        case 'edit':
+            console.log(e.target.dataset.uid)
+            updateToDoController(e.target.dataset.uid)
+            break
+        default:
+            null
+    }
+}
